c2a48f07afdac10b9197cd6db7aa7527fe321d3f
lexus9425 <68475116+lexus9425@users.noreply.github.com>
2022-07-20 11:50:36 -0400

    Final Commit -Import contacts.js into App.jsx -Render new Cards using data from contacts.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import contacts from "../contacts";
 
 function App() {
   function Card(props) {
@@ -24,10 +25,22 @@ function App() {
     <div>
       <h1 className="heading">My Contacts</h1>
       <Card
-        name="Beyonce"
-        img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-        tel="+123 456 789"
-        email="b@beyonce.com"
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
       />
     </div>
   );
